b6453159d72ad24e8f6b38b4c3f49b92d9edc64a
1999b <1999b@AlexPC>
2019-01-13 22:10:59 -0600

    Added Code For Items

--- BusterCraft2/src/main/java/busterbrown99/bustercraft2/proxy/CommonProxy.java
+++ BusterCraft2/src/main/java/busterbrown99/bustercraft2/proxy/CommonProxy.java
@@ -1,5 +1,8 @@
 package busterbrown99.bustercraft2.proxy;
 
-public class CommonProxy{
+import net.minecraft.item.Item;
 
+public class CommonProxy{
+	public void registerItemRenderer(Item item, int meta, String id) {
+	}
 }
